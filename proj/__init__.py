acf33bb8bc48dd4d5f67efdfb415bc4df7c38f68
Lars Yencken <lars@yencken.org>
2020-05-14 19:53:07 +0200

    Set proj to archive to a compressed folder by default
    At the moment, only a single compression format is supported (tbz2), and compression is enabled by default.
    
    The underlying motivation is that I use this folder in Dropbox, and Dropbox has a number-of-files performance criteria that doesn't fit well with deep back-histories of projects.

--- proj/__init__.py
+++ proj/__init__.py
@@ -23,6 +23,9 @@ import click
 
 PROJ_ARCHIVE = os.environ.get("PROJ_ARCHIVE")
 
+COMPRESS_FORMAT = "bztar"
+COMPRESS_EXT = ".tar.bz2"
+
 
 def bail(message: str) -> NoReturn:
     click.echo(message, err=True)
@@ -58,7 +61,8 @@ def main():
 @click.command()
 @click.argument("folder", nargs=-1)
 @click.option("-n", "--dry-run", is_flag=True, help="Don't make any changes")
-def archive(folder: List[str], dry_run: bool = False):
+@click.option("--no-compress", is_flag=True, help="Do not compress the folder")
+def archive(folder: List[str], dry_run: bool = False, no_compress: bool = False):
     "Move an active project to the archive."
     # error handling on archive_dir already done in main()
 
@@ -67,7 +71,7 @@ def archive(folder: List[str], dry_run: bool = False):
             bail("folder does not exist: " + f)
 
     archive_dir = get_archive_dir()
-    _archive_safe(folder, archive_dir, dry_run=dry_run)
+    _archive_safe(folder, archive_dir, dry_run=dry_run, compress=not no_compress)
 
 
 def _last_modified(folder: str) -> dt.datetime:
@@ -99,7 +103,9 @@ def _to_quarter(t: dt.datetime) -> Tuple[str, str]:
     return str(t.year), "q" + str(1 + (t.month - 1) // 3)
 
 
-def _archive_safe(folders: List[str], archive_dir: str, dry_run: bool = False) -> None:
+def _archive_safe(
+    folders: List[str], archive_dir: str, dry_run: bool = False, compress: bool = False
+) -> None:
     for folder in folders:
         t = _last_modified(folder)
         year, quarter = _to_quarter(t)
@@ -107,9 +113,29 @@ def _archive_safe(folders: List[str], archive_dir: str, dry_run: bool = False) -
 
         print(folder, "-->", dest_dir)
         if not dry_run:
-            parent_dir = os.path.dirname(dest_dir)
-            _mkdir(parent_dir)
-            shutil.move(folder, dest_dir)
+            if compress:
+                _compress_and_archive(folder, dest_dir)
+            else:
+                _archive_folder(folder, dest_dir)
+
+
+def _archive_folder(folder, dest_dir):
+    parent_dir = os.path.dirname(dest_dir)
+    _mkdir(parent_dir)
+    shutil.move(folder, dest_dir)
+
+
+def _compress_and_archive(src_folder, dst_folder):
+    parent_dir = os.path.dirname(dst_folder)
+    _mkdir(parent_dir)
+
+    try:
+        shutil.make_archive(dst_folder, COMPRESS_FORMAT, src_folder)
+    except Exception as e:
+        shutil.rm(dst_folder + COMPRESS_EXT)
+        raise e
+
+    shutil.rmtree(src_folder)
 
 
 def _mkdir(p: str) -> None:
@@ -155,18 +181,36 @@ def restore(folder: str) -> None:
     if os.path.isdir(folder):
         bail("a folder of the same name already exists!")
 
+    source = _find_restore_match(folder)
+
+    if source.endswith(COMPRESS_EXT):
+        nice_source = source[: -len(COMPRESS_EXT)]
+        print(nice_source, "-->", folder)
+        shutil.unpack_archive(source, "./" + folder)
+        os.unlink(source)
+    else:
+        print(source, "-->", folder)
+        shutil.move(source, ".")
+
+
+def _find_restore_match(folder):
     archive_dir = get_archive_dir()
     pattern = os.path.join(archive_dir, "*", "*", folder)
     matches = glob.glob(pattern)
     if not matches:
-        bail("no project matches: " + folder)
+        # try compressed
+        pattern = os.path.join(archive_dir, "*", "*", folder + COMPRESS_EXT)
+        matches = glob.glob(pattern)
+
+        if not matches:
+            bail("no project matches: " + folder)
 
     if len(matches) > 1:
         print("Warning: multiple matches, picking the most recent", file=sys.stderr)
 
     source = sorted(matches)[-1]
-    print(source, "-->", folder)
-    shutil.move(source, ".")
+
+    return source
 
 
 main.add_command(archive)
