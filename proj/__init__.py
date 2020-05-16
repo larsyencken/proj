1cde4b498ec7ada102de1cfde8d9383059df92ab
Lars Yencken <lars@yencken.org>
2020-05-16 21:55:38 +0200

    Add type hints to proj

--- proj/__init__.py
+++ proj/__init__.py
@@ -15,6 +15,8 @@ import os
 import shutil
 import sys
 from functools import reduce
+import datetime as dt
+from typing import NoReturn, Tuple, List, Iterator
 
 import arrow
 import click
@@ -22,9 +24,19 @@ import click
 PROJ_ARCHIVE = os.environ.get("PROJ_ARCHIVE")
 
 
-def bail(message):
+def bail(message: str) -> NoReturn:
     click.echo(message, err=True)
     sys.exit(1)
+
+
+def get_archive_dir() -> str:
+    if PROJ_ARCHIVE is None:
+        bail("please set PROJ_ARCHIVE to your archive's location")
+
+    if not os.path.isdir(PROJ_ARCHIVE):
+        bail("archive directory does not exist: " + PROJ_ARCHIVE)
+
+    return PROJ_ARCHIVE
 
 
 @click.group()
@@ -40,17 +52,13 @@ def main():
     proj needs an archive directory specified by the PROJ_ARCHIVE
     environment variable.
     """
-    if PROJ_ARCHIVE is None:
-        bail("please set PROJ_ARCHIVE to your archive's location")
-
-    if not os.path.isdir(PROJ_ARCHIVE):
-        bail("archive directory does not exist: " + PROJ_ARCHIVE)
+    pass
 
 
 @click.command()
 @click.argument("folder", nargs=-1)
 @click.option("-n", "--dry-run", is_flag=True, help="Don't make any changes")
-def archive(folder, dry_run=False):
+def archive(folder: List[str], dry_run: bool = False):
     "Move an active project to the archive."
     # error handling on archive_dir already done in main()
 
@@ -58,10 +66,11 @@ def archive(folder, dry_run=False):
         if not os.path.exists(f):
             bail("folder does not exist: " + f)
 
-    _archive_safe(folder, PROJ_ARCHIVE, dry_run=dry_run)
+    archive_dir = get_archive_dir()
+    _archive_safe(folder, archive_dir, dry_run=dry_run)
 
 
-def _last_modified(folder):
+def _last_modified(folder: str) -> dt.datetime:
     try:
         return max(
             _time_modified(f) for f in _iter_files(folder) if not os.path.islink(f)
@@ -71,7 +80,7 @@ def _last_modified(folder):
         bail("no files in folder: " + folder)
 
 
-def _iter_files(folder):
+def _iter_files(folder: str) -> Iterator[str]:
     if os.path.isdir(folder):
         for dirname, subdirs, filenames in os.walk(folder):
             for basename in filenames:
@@ -82,15 +91,15 @@ def _iter_files(folder):
         yield folder
 
 
-def _time_modified(filename):
+def _time_modified(filename: str) -> arrow.Arrow:
     return arrow.get(os.stat(filename).st_mtime)
 
 
-def _to_quarter(t):
+def _to_quarter(t: dt.datetime) -> Tuple[str, str]:
     return str(t.year), "q" + str(1 + (t.month - 1) // 3)
 
 
-def _archive_safe(folders, archive_dir, dry_run=False):
+def _archive_safe(folders: List[str], archive_dir: str, dry_run: bool = False) -> None:
     for folder in folders:
         t = _last_modified(folder)
         year, quarter = _to_quarter(t)
@@ -103,7 +112,7 @@ def _archive_safe(folders, archive_dir, dry_run=False):
             shutil.move(folder, dest_dir)
 
 
-def _mkdir(p):
+def _mkdir(p: str) -> None:
     "The equivalent of 'mkdir -p' in shell."
     isdir = os.path.isdir
 
@@ -120,31 +129,34 @@ def _mkdir(p):
 
 @click.command()
 @click.argument("pattern", nargs=-1)
-def list(pattern=()):
+def list(pattern: List[str]) -> None:
     "List the contents of the archive directory."
     # strategy: pick the intersection of all the patterns the user provides
     globs = ["*{0}*".format(p) for p in pattern] + ["*"]
+    archive_dir = get_archive_dir()
 
-    matches = []
-    offset = len(PROJ_ARCHIVE) + 1
+    match_sets = []
+    offset = len(archive_dir) + 1
     for suffix in globs:
-        glob_pattern = os.path.join(PROJ_ARCHIVE, "*", "*", suffix)
-        matches.append(set(f[offset:] for f in glob.glob(glob_pattern)))
+        glob_pattern = os.path.join(archive_dir, "*", "*", suffix)
+        match_set = set(f[offset:] for f in glob.glob(glob_pattern))
+        match_sets.append(match_set)
 
-    matches = reduce(lambda x, y: x.intersection(y), matches)
+    final_set = reduce(lambda x, y: x.intersection(y), match_sets)
 
-    for m in sorted(matches):
+    for m in sorted(final_set):
         print(m)
 
 
 @click.command()
 @click.argument("folder")
-def restore(folder):
+def restore(folder: str) -> None:
     "Restore a project from the archive."
     if os.path.isdir(folder):
         bail("a folder of the same name already exists!")
 
-    pattern = os.path.join(PROJ_ARCHIVE, "*", "*", folder)
+    archive_dir = get_archive_dir()
+    pattern = os.path.join(archive_dir, "*", "*", folder)
     matches = glob.glob(pattern)
     if not matches:
         bail("no project matches: " + folder)
